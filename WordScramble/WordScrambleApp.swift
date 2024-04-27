887d51c4ac34fde92b18083c9ed157d66dd19ec0
Igor Shcherba <igor.shcherba@callstack.com>
2024-04-27 19:24:55 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Igor Shcherba on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
